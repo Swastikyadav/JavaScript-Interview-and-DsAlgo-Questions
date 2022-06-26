c44fd626026ee30c44aa02385115e9ad67b5ce12
Swastikyadav <swastikyadav.dev@gmail.com>
2022-06-26 07:07:02 +0530

    Add anagram problem and solution

---
+++ 7-anagrams.js
@@ -0,0 +1,53 @@
+/*
+  Check to see if two provided strings are anagarams of eachother.
+  
+  One string is anagram of another if it uses the same characters in the same quanity. Only consider characters, not spaces or punctuation.
+
+  Consider capital letters to be same as lower case letters.
+
+  --Examples
+  
+  anagrams('rail safety', 'fairy tales') -> True
+  anagrams('RAIL! SAFETY!', 'fairy tales') -> True
+  anagrams('Hi there', 'Bye there') -> False
+*/
+
+// Here we will use the same charMap technique, we used in 4-maxChar.js problem.
+
+function anagrams(stringA, stringB) {
+  const stringACharMap = characterMapCreatorFunction(stringA);
+  const stringBCharMap = characterMapCreatorFunction(stringB);
+
+  // If keys length of both charMap object is diff return false.
+  if (Object.keys(stringACharMap).length !== Object.keys(stringBCharMap).length) {
+    return false;
+  }
+
+  // If both charMap object value is diff for same key return false.
+  for (let key in stringACharMap) {
+    if (stringACharMap[key] !== stringBCharMap[key]) {
+      return false;
+    }
+  }
+
+  // Else return true.
+  return true;
+}
+
+// Helper function to build charMap object.
+function characterMapCreatorFunction(str) {
+  const charMap = {};
+  const letterRegex = /^[a-zA-Z]+$/;
+
+  str.split("").forEach(char => {
+    if (letterRegex.test(char)) {
+      charMap[char.toLowerCase()] = charMap[char.toLowerCase()] + 1 || 1;
+    }
+  });
+
+  return charMap;
+}
+
+console.log(anagrams('rail safety', 'fairy tales'));
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales'));
+console.log(anagrams('Hi there', 'Bye there'));
