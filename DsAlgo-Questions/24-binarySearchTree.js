db5350c8ee3c87562da4b6aceefffeebd2716074
Swastikyadav <swastikyadav.dev@gmail.com>
2022-07-10 15:35:23 +0530

    Implement the insert method on binary search tree node class

---
+++ DsAlgo-Questions/24-binarySearchTree.js
@@ -0,0 +1,28 @@
+/* Binary Search Tree !== Binary Tree
+            |                   |
+          Sorted            Not Sorted
+
+  1. Implement the node class to create a binary search tree. The constructor should initialize values 'data', 'left', and 'right'.
+
+  2. Implement the 'insert' method for the Node class. Insert should accept an argument 'data', then create and insert a new node at the appropriate location in the tree.
+*/
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(data) {
+    if (data < this.data && this.left) {
+      this.left.insert(data);
+    } else if (data < this.data) {
+      this.left = new Node(data);
+    } else if (data > this.data && this.right) {
+      this.right.insert(data);
+    } else if (data > this.data) {
+      this.right = new Node(data);
+    }
+  }
+}
