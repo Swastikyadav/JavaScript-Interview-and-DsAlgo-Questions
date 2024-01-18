b53fb7d7c7e477af10936cd13f5b6e03082508d8
Swastikyadav <swastikb380@gmail.com>
2024-01-18 21:23:00 +0530

    Fix wrong solution of 05 problem

--- Algo-Patterns/sliding-window-pattern/05-non-repeating-substr.js
+++ Algo-Patterns/sliding-window-pattern/05-non-repeating-substr.js
@@ -42,9 +42,69 @@ function non_repeat_substring(str) {
   return maxLength;
 }
 
-console.log(non_repeat_substring("aabccbb"));
-console.log(non_repeat_substring("abbbb"));
-console.log(non_repeat_substring("abccde"));
+// console.log(non_repeat_substring("aabccbb"));
+// console.log(non_repeat_substring("abbbb"));
+// console.log(non_repeat_substring("abccde"));
+console.log(non_repeat_substring("abcdaefghijkl"), "#0");
+
+// -------- Alternate Solution ----------
+// The above solution algo fails for last testcase. The 2 solutions below are correct.
+
+// Solution 1
+function noRepeatCharLen1(str) {
+  let j = 0;
+  let i = 0;
+  const frequencyTracker = {};
+  let longestSubStrLen = 0;
+
+  while(j < str.length) {
+      if (!frequencyTracker.hasOwnProperty(str[j])) {
+          frequencyTracker[str[j]] = 1;
+          longestSubStrLen = Math.max(longestSubStrLen, j - i + 1);
+      } else {
+          frequencyTracker[str[j]]++;
+      }
+
+      while(frequencyTracker[str[j]] > 1) {
+          frequencyTracker[str[i]]--
+          if (frequencyTracker[str[i]] === 0) {
+              delete frequencyTracker[str[i]];
+          }
+
+          i++;
+      }
+
+      j++;
+  };
+
+  return longestSubStrLen;
+}
+
+// Solution #2
+function noRepeatCharLen2(str) {
+  let windowStart = 0,
+      maxLength = 0,
+      charIndexMap = {};
+
+  for(let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+      const rightChar = str[windowEnd];
+
+      if (rightChar in charIndexMap) {
+          windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+      }
+
+      charIndexMap[rightChar] = windowEnd;
+
+      maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+  }
+
+  return maxLength;
+}
+
+console.log(noRepeatCharLen1("abcdaefghijkl"), "#1");
+console.log(noRepeatCharLen2("abcdaefghijkl"), "#2");
+
+// a b c d a e f g h i j k l
 
 /*
 Time Complexity #
